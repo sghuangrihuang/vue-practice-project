9a3f3e2ab93d12419df1dfb69a9a1f6ac74cd12e
sghuangrihuang <287545836@qq.com>
2017-07-29 16:10:36 +0800

    完善user用户页的功能模块

--- src/components/user/user.vue
+++ src/components/user/user.vue
@@ -11,8 +11,17 @@
         <mu-icon slot="left" value="email"/>
         <mu-badge :content="loginInfo.email || '未公开'" primary slot="after"/>
       </mu-list-item>
-      <mu-list-item title="博客">
-        <mu-icon slot="left" value="address"/>
+      <mu-list-item title="仓库" @click="reposClick">
+        <mu-icon slot="left" value="folder"/>
+        <mu-badge :content="`${loginInfo.public_repos}`" slot="after" primary/>
+      </mu-list-item>
+      <mu-list-item title="粉丝"  @click="followerClick">
+        <mu-icon slot="left" value="person_pin"/>
+        <mu-badge :content="`${loginInfo.followers}`" slot="after" primary/>
+      </mu-list-item>
+      <mu-list-item title="关注">
+        <mu-icon slot="left" value="favorite"/>
+        <mu-badge :content="`${loginInfo.following}`" slot="after" primary/>
       </mu-list-item>
       <mu-divider shallowInset/>
       <mu-list-item title="退出" @click="open">
@@ -20,20 +29,42 @@
       </mu-list-item>
     </mu-list>
     <mu-dialog :open="dialog" title="是否退出" @close="close">
-      <mu-flat-button slot="actions" @click="close" primary label="取消"/>
-      <mu-flat-button slot="actions" primary @click="signOut" label="确定"/>
-  </mu-dialog>
+        <mu-flat-button slot="actions" @click="close" primary label="取消"/>
+        <mu-flat-button slot="actions" primary @click="signOut" label="确定"/>
+    </mu-dialog>
+    <transition name="move">
+      <div class="followersList" v-show="followerFlag" @click='followerFlag = false'>
+        <mu-list>
+          <mu-sub-header inset >followers</mu-sub-header>
+          <mu-list-item v-for="item in followers" :key="item.id" :title="item.name || item.login" :describeText="item.html_url">
+            <mu-avatar :src="item.avatar_url" slot="leftAvatar" />
+            <mu-icon value="info" slot="right"/>
+          </mu-list-item>
+        </mu-list>
+      </div>
+    </transition>
+    <transition name="fade">
+      <div class="loading" v-show="loadingFlag">
+        <mu-circular-progress :size="60" :strokeWidth="3" />
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
 import {mapState, mapActions} from 'vuex'
+import axios from 'axios'
 
 export default {
   name: 'user',
   data() {
     return {
-      dialog: false
+      dialog: false,
+      followerFlag: false,
+      followers: {
+        type: Object
+      },
+      loadingFlag: false
     }
   },
   computed: {
@@ -60,10 +91,64 @@ export default {
       this.$router.push({
         path: '/login'
       })
+    },
+    followerClick() {
+      if (!this.loginInfo.followers) {
+        return
+      }
+      let url = `https://api.github.com/users/${this.loginInfo.login}/followers`
+      this.loadingFlag = true
+      axios.get(url).then((res) => {
+        this.followers = res.data
+        console.log(this.followers)
+        this.loadingFlag = false
+        this.followerFlag = true
+      }).catch((res) => {
+        console.log(res)
+      })
+    },
+    reposClick() {
+      let url = `https://api.github.com/users/${this.loginInfo.login}/repos`
+      axios.get(url).then((res) => {
+        // console.log(res.data)
+      }).catch((res) => {
+        console.log(res)
+      })
     }
   }
 }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+.followersList
+  position: absolute
+  top:0px
+  left: 0
+  right 0
+  bottom:0px
+  z-index: 30
+  background-color: #FFF
+  transform:translate3d(0,0,0)
+  &.move-enter-active,&.move-leave-active
+    transition: all 0.2s linear
+  &.move-enter,&.move-leave-active
+    transform:translate3d(100%,0,0)
+.loading
+  position: absolute
+  top:0px
+  left: 0
+  right 0
+  text-align center
+  bottom:0px
+  z-index: 30
+  display flex
+  flex-direction column
+  align-items center
+  justify-content center
+  opacity 1
+  background-color: rgba(0, 0, 0, .5)
+  &.fade-enter-active, &.fade-leave-active
+    transition: all 0.2s linear
+  &.fade-enter,&.fade-leave-active
+    opacity 0
 </style>
